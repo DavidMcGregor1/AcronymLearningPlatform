c0b4cedec52be9a9aa84a8961b9f291588e996e3
David McGregor <david@plisky.net>
2024-02-08 16:20:50 +0000

    added full comment documentation to the learnPageLogin javascript file

--- src/main/resources/static/javascript/learnPageLogin.js
+++ src/main/resources/static/javascript/learnPageLogin.js
@@ -8,40 +8,20 @@ const loginButtonHdr = document.getElementById("login-button-hdr");
 const logoutButtonHdr = document.getElementById("logout-button-hdr");
 logoutButtonHdr.classList.add("hidden");
 
+const loginErrorMessage = document.getElementById("login-error-div");
+loginErrorMessage.classList.add("hidden");
+
+const addAcronymErrorMessage = document.getElementById("add-acronym-error-div");
+addAcronymErrorMessage.classList.add("hidden");
+
 var editAddOrLogin;
 
+// Store web token in local storage if the response from the server jwt is not null
 function isLoggedIn() {
   return localStorage.getItem("jwt") !== null;
 }
 
-function logout() {
-  localStorage.removeItem("jwt");
-  window.location.reload();
-}
-
-function makeAuthenticatedRequest(endpoint, method, data) {
-  const jwt = localStorage.getItem("jwt");
-  if (!jwt) {
-    console.error("JWT not found. User is not logged in.");
-    return;
-  }
-
-  fetch(endpoint, {
-    method: method,
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${jwt}`,
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => {
-      console.log("Inside handle response ");
-    })
-    .catch((error) => {
-      console.error("Error making authenticated request:", error);
-    });
-}
-
+// Displays the login page when the user clicks the login button
 loginButtonHdr.addEventListener("click", () => {
   console.log("Clicked the login button in the header");
   editAddOrLogin = "login";
@@ -49,16 +29,24 @@ loginButtonHdr.addEventListener("click", () => {
   descriptionSection.classList.add("hidden");
 });
 
+// Remove jwt from local storage and reloads the page
+function logout() {
+  localStorage.removeItem("jwt");
+  window.location.reload();
+}
+
+// Logs the user out when the user clicks the logout button
+logoutButtonHdr.addEventListener("click", () => {
+  console.log("clicked logout button");
+  logout();
+});
+
+// Only remove login button and show logout button if the user is logged in
 if (isLoggedIn()) {
-  console.log("User is logged in (outside any method)");
   replaceLoginButtonWithLogoutButton();
 }
 
-const loginErrorMessage = document.getElementById("login-error-div");
-const addAcronymErrorMessage = document.getElementById("add-acronym-error-div");
-loginErrorMessage.classList.add("hidden");
-addAcronymErrorMessage.classList.add("hidden");
-
+// Login button on form
 const loginButton = document.getElementById("login-button");
 loginButton.addEventListener("click", () => {
   console.log("clicked login button after entering fields");
@@ -74,34 +62,40 @@ loginButton.addEventListener("click", () => {
         if (jwt) {
           localStorage.setItem("jwt", jwt);
 
+          // if the user clicked the add acronym button to get to this login form then show the add acronym form
           if (editAddOrLogin === "add") {
             replaceLoginButtonWithLogoutButton();
             additLogin.classList.add("hidden");
             addAcronymContainer.classList.remove("hidden");
+            // if the user clicked the edit acronym button to get to this login form then show the edit acronym form
           } else if (editAddOrLogin === "edit") {
             replaceLoginButtonWithLogoutButton();
             additLogin.classList.add("hidden");
             editDescriptionSection.classList.remove("hidden");
+            // if the user clicked the login button to get to this form then show login successfull page for one second and reload the page so they see the description form again
           } else if (editAddOrLogin === "login") {
             loggedInTempSection.classList.remove("hidden");
             additLogin.classList.add("hidden");
-
             setTimeout(function () {
               loggedInTempSection.classList.add("hidden");
               description.classList.remove("hidden");
               window.location.reload();
             }, 1000);
           }
-        } else {
-          console.log("Login failed. Display error message.");
+        }
+        // if the login was incorrect then show the login error message
+        else {
           loginErrorMessage.classList.remove("hidden");
         }
-      } else {
+      }
+      // if there is an issue getting a response from the server then log out the error (dev use)
+      else {
         console.error("Error: ", xhr.status);
       }
     }
   };
 
+  // Send a post request to the /login api with the users inputted username and password
   xhr.open("POST", "/login", true);
   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   const requestBody =
@@ -112,11 +106,7 @@ loginButton.addEventListener("click", () => {
   xhr.send(requestBody);
 });
 
-logoutButtonHdr.addEventListener("click", () => {
-  console.log("clicked logout button");
-  logout();
-});
-
+// Replaces the login button with the logout button if this function is called (should only be called if the user is logged in)
 function replaceLoginButtonWithLogoutButton() {
   console.log("called replaceLoginWithLogout method");
   const loginButtonHdr = document.getElementById("login-button-hdr");
@@ -125,6 +115,7 @@ function replaceLoginButtonWithLogoutButton() {
   logoutButtonHdr.classList.remove("hidden");
 }
 
+// Checks if the user is logged in when they click the add button. If they are, they see the add form, if they are not, they see the login form.
 const addNewAcronymBtn = document.getElementById("add-new-acronym-btn");
 addNewAcronymBtn.addEventListener("click", () => {
   editAddOrLogin = "add";
@@ -138,6 +129,7 @@ addNewAcronymBtn.addEventListener("click", () => {
   }
 });
 
+// Checks if the user is logged in when they click the edit button. If they are, they see the edit form, if they are not, they see the login form.
 const editDescriptionBtn = document.getElementById("edit-description-button");
 editDescriptionBtn.addEventListener("click", () => {
   editAddOrLogin = "edit";
