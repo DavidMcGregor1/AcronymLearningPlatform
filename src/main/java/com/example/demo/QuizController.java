fe3dc468a8ae89dcd6c16b791b2de1695f55036b
David McGregor <david@plisky.net>
2024-02-13 10:37:36 +0000

    added constructor to QuizController with acronyms repository

--- src/main/java/com/example/demo/QuizController.java
+++ src/main/java/com/example/demo/QuizController.java
@@ -4,4 +4,9 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class QuizController {
+    public QuizController(AcronymsRepository a) {
+        repositoryAcronyms = a;
+    }
+
+    private AcronymsRepository repositoryAcronyms;
 }
