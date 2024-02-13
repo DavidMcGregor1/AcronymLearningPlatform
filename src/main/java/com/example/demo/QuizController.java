df9f6e38219ae9aa4ba9c74ed9cb846d481bfb42
David McGregor <david@plisky.net>
2024-02-13 13:24:24 +0000

    added first /getQuestion endpoint method

--- src/main/java/com/example/demo/QuizController.java
+++ src/main/java/com/example/demo/QuizController.java
@@ -1,6 +1,15 @@
 package com.example.demo;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class QuizController {
@@ -9,4 +18,24 @@ public class QuizController {
     }
 
     private AcronymsRepository repositoryAcronyms;
+
+    @ResponseStatus(value = HttpStatus.OK)
+    @ResponseBody
+    @GetMapping(path = "/getQuestion", consumes = "application/json", produces = "application/json")
+    public String getQuestion(Model model) {
+        List<Acronyms> allAcronyms = repositoryAcronyms.findAll();
+        String result = "All Aronyms ---> ";
+        for (int i = 0; i < allAcronyms.stream().count(); i++) {
+            Acronyms a = allAcronyms.get(i);
+            if (a != null ) {
+                result += a.getAcronym() + ", ";
+            }
+        }
+        Random random = new Random();
+        int randomAcronymIndex = random.nextInt(allAcronyms.toArray().length) + 1;
+        String fullQuestion = "What does " + ((Acronyms)allAcronyms.toArray()[randomAcronymIndex]).getAcronym() + " stand for?";
+//        return fullQuestion;
+        return result;
+    }
+
 }
