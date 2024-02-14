82e3d60ee1dee504baa5a01427c8166cb8532fc8
David McGregor <david@plisky.net>
2024-02-14 15:32:44 +0000

    added highlight functionality to all button to remove category highlight and remove all button highlight when category option is clicked

--- src/main/resources/static/javascript/quizMenu.js
+++ src/main/resources/static/javascript/quizMenu.js
@@ -1,7 +1,37 @@
 const optionButtons = document.querySelectorAll(".option-button");
+const categoriesButton = document.querySelectorAll(".categories-button");
+const allButton = document.getElementById("all-button");
+
+let allSelected = false;
+
 optionButtons.forEach((button) => {
   button.addEventListener("click", () => {
     optionButtons.forEach((b) => b.classList.remove("clicked"));
     button.classList.add("clicked");
   });
+});
+
+categoriesButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    const isButtonClicked = button.classList.contains("clicked");
+    const group = button.getAttribute("data-group");
+    allButton.classList.remove("clicked");
+
+    if (button === allButton) {
+      categoriesButton.forEach((b) => b.classList.remove("clicked"));
+    } else {
+      categoriesButton.forEach((b) => {
+        if (b.getAttribute("data-group") === group) {
+          b.classList.remove("clicked");
+        }
+      });
+    }
+    button.classList.toggle("clicked", !isButtonClicked);
+  });
+});
+
+allButton.addEventListener("click", () => {
+  categoriesButton.forEach((button) => button.classList.remove("clicked"));
+  allButton.classList.add("clicked");
+  allSelected = allButton.classList.contains("clicked");
 });
