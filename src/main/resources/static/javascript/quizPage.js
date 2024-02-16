c43976719645cf610a5f0498d1fd99eff393f3b9
David McGregor <david@plisky.net>
2024-02-16 15:42:24 +0000

    added logic to display score at the end of the quiz but still need to style

--- src/main/resources/static/javascript/quizPage.js
+++ src/main/resources/static/javascript/quizPage.js
@@ -33,16 +33,25 @@ function getQuestionsBasedOnSelections() {
   xhr.send();
 }
 
+let globalScore = 0;
+
 function displayQuestion() {
   const questionElement = document.getElementById("question");
   const optionsContainer = document.querySelector(".options");
   const questionNumber = document.getElementById("question-number");
-  questionNumber.innerText = `Question ${currentQuestionIndex + 1} of ${
-    shuffledQuestions.length
-  }`;
-  optionsContainer.innerHTML = "";
+  const quizOverMessages = document.querySelectorAll(".quiz-over");
 
-  if (shuffledQuestions.length > 0) {
+  if (!questionElement || !optionsContainer || !questionNumber) {
+    console.error("One or more required elements not found.");
+    return;
+  }
+
+  if (currentQuestionIndex < shuffledQuestions.length) {
+    questionNumber.innerText = `Question ${currentQuestionIndex + 1} of ${
+      shuffledQuestions.length
+    }`;
+    optionsContainer.innerHTML = "";
+
     const currentQuestion = shuffledQuestions[currentQuestionIndex];
     questionElement.textContent = currentQuestion.question;
 
@@ -56,9 +65,14 @@ function displayQuestion() {
       optionsContainer.appendChild(button);
     });
   } else {
-    console.error("Shuffled questions array is empty or undefined.");
+    // Quiz over
+    quizOverMessages.forEach((message) => message.classList.remove("hidden"));
+    document.getElementById("score").innerText =
+      "Score: " + globalScore + "/" + shuffledQuestions.length;
   }
 }
+
+console.log("glob 1 " + globalScore);
 
 function selectAnswer(selectedIndex) {
   const currentQuestion = shuffledQuestions[currentQuestionIndex];
@@ -70,12 +84,29 @@ function selectAnswer(selectedIndex) {
 
   if (selectedIndex === correctAnswerIndex) {
     selectedButton.classList.add("correct");
+    globalScore += 1;
+    console.log("gs " + globalScore);
   } else {
     selectedButton.classList.add("wrong");
   }
+
+  console.log("gs " + globalScore);
 
   setTimeout(() => {
     currentQuestionIndex++;
     displayQuestion();
   }, 700);
 }
+
+// function calculateScore() {
+//   let score = 0;
+//   shuffledQuestions.forEach((question) => {
+//     const correctAnswerIndex = question.options.indexOf(question.answer);
+//     const selectedButton =
+//       document.querySelector(".options").children[correctAnswerIndex];
+//     if (selectedButton && selectedButton.classList.contains("correct")) {
+//       score++;
+//     }
+//   });
+//   return score;
+// }
