01a28e68a5cd223773e5d1e1f60e1d4ef0b372bd
David McGregor <david@plisky.net>
2024-02-27 08:50:38 +0000

    resting branch reset

--- deploy.ps1
+++ deploy.ps1
@@ -4,12 +4,12 @@
 # Need to manually build the jar file and then run this
 
 # # Build Docker Image
- docker build -t davidsregistry01.azurecr.io/acronym-image-1:acVersion-6 .
+ docker build -t davidsregistry01.azurecr.io/acronym-image-1:acVersion-7 .
 #
 # # Login to Azure Container Registry
 az acr login --name davidsregistry01.azurecr.io
 #
 # # Push Docker Image to ACR
- docker push davidsregistry01.azurecr.io/acronym-image-1:acVersion-6
+ docker push davidsregistry01.azurecr.io/acronym-image-1:acVersion-7
 
 
---
+++ src/test/java/com/example/demo/UsersControllerTest.java
@@ -0,0 +1,68 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UsersControllerTest {
+
+    @Mock
+    private UsersRepository usersRepositoryMock;
+
+    @InjectMocks
+    private UsersController usersController;
+
+    @Test
+    void testGetAllUsers() {
+        List<Users> users = new ArrayList<>();
+        users.add(new Users("user1", "password1"));
+        users.add(new Users("user2", "password2"));
+
+        when(usersRepositoryMock.findAll()).thenReturn(users);
+
+        String result = usersController.getAllUsers();
+
+        assertTrue(result.contains("user1"));
+        assertTrue(result.contains("user2"));
+        assertTrue(result.contains("password1"));
+        assertTrue(result.contains("password2"));
+    }
+
+    @Test
+    void testLoginFailure() {
+        String submittedUsername = "david";
+        String submittedPassword = "invalid_password";
+
+        Users user = new Users("david", "david");
+        when(usersRepositoryMock.findByUsername(submittedUsername)).thenReturn(Optional.of(user));
+
+        String jwt = usersController.login(submittedUsername, submittedPassword);
+
+        assertNull(jwt, "JWT should be null for invalid credentials");
+    }
+
+    @Test
+    void testLoginSuccess() {
+        String submittedUsername = "david";
+        String submittedPassword = "david";
+
+        Users user = new Users("david", "david");
+        when(usersRepositoryMock.findByUsername(submittedUsername)).thenReturn(Optional.of(user));
+
+        String jwt = usersController.login(submittedUsername, submittedPassword);
+
+        assertNotNull(jwt, "JWT should not be null for valid credentials");
+    }
+
+}
+
